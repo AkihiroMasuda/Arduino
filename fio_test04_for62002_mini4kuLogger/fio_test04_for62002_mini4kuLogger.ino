434d22fbbf109f84a699fb9e601a744d1238199f
hoge <hoge@hoge>
2013-10-14 16:13:42 +0900

    ミニ四駆ロガー。速度測定間隔を50msとすることで通信を安定させた。

--- fio_test04_for62002_mini4kuLogger/fio_test04_for62002_mini4kuLogger.ino
+++ fio_test04_for62002_mini4kuLogger/fio_test04_for62002_mini4kuLogger.ino
@@ -91,7 +91,8 @@ int detectPitch(BYTE flg, BYTE *buf, BYTE curInd)
   //	const BYTE shikii = 0x20;
   //	const BYTE shikii = 146;
 //  const BYTE shikii = 253;
-  const BYTE shikii = 150;
+//  const BYTE shikii = 150;
+  const BYTE shikii = 100;
 
   buf[0] = getAnalogData();
   buf[1] = getAnalogData();
@@ -233,9 +234,16 @@ void setup()
 }
 
 
-int cnt = 0;
+unsigned long cnt = 0;
 unsigned long tc_old =  0;
 unsigned long dts[5];
+
+int flg = 0;
+
+//unsigned long tc_old =  0;
+//unsigned long dts[5];
+unsigned long beforeTime = 0;
+
 
 void loop()
 {
@@ -244,38 +252,33 @@ void loop()
     delay(5000);
     //LED点灯
     digitalWrite(DPORT_LED,HIGH);
+  }else{
+    
+    // 一定間隔で送信
+    //アナログポート読み込み
+//    int val[] = {analogRead(APORT_X), analogRead(APORT_Y), analogRead(APORT_Z)};    // アナログピンを読み取る
+//    int val[] = {analogRead(APORT_X), 0, 0};    // アナログピンを読み取る
+//    int val[] = {0,0,0};    // アナログピンを読み取る
+    unsigned long curTime = millis();
+    if (curTime-beforeTime >= 50){
+      // 50msに一回速度測定するようにする。
+      // この閾値無しで測定すると、送信間隔が約15ms程度となり、Bluetoothでの送信処理が輻輳する。
+      // なのでとりあえずこの間隔で。
+      beforeTime = curTime;
+
+      unsigned long dt = calRotUS();
+      float vel = (0.03f*3.14159)/((float)(dt)/1000.f/1000.f)*3.6f;//km/h, 直径3cmと仮定
+
+      memset(sendText, 0, sizeof(sendText));
+      sprintf(sendText, "%ld,%ld,%d", curTime, dt, (int)(vel));
+      mySerial.println(sendText);
+      cnt++;
+      
+      flg = flg==1 ? 0 : 1;
+      digitalWrite(DPORT_LED,flg==1?LOW:HIGH);
+      
+    }
   }
-
-  // タイヤ一回転の時間を測定[μ秒]
-  unsigned long dt = calRotUS();
-//  unsigned long dt = 20000;
-  unsigned long curTime = millis();
-  float vel = (0.03f*3.14159)/((float)(dt)/1000.f/1000.f)*3.6f;//km/h, 直径3cmと仮定
-//  double vel = 3.14159;
-  memset(sendText, 0, sizeof(sendText));
-  sprintf(sendText, "%ld,%ld,%d\n", curTime, dt, (int)(vel));
-  mySerial.println(sendText);
-  
-//  //  unsigned long tc = millis();
-//  unsigned long tc = micros();
-//  unsigned long dt = tc - tc_old;
-//  tc_old = tc;
-//  if (cnt < 5){
-//    dts[cnt] = dt;  
-//    ++cnt;
-//    //    delay(0);
-//  }
-//  else{
-//    cnt = 0;
-//    memset(sendText, 0, sizeof(sendText));
-//    sprintf(sendText, "dts : %ld,%ld,%ld,%ld,%ld\n", dts[0], dts[1], dts[2], dts[3], dts[4]);
-//    mySerial.println(sendText);
-//  }
-
-  //LED点灯
-  //    digitalWrite(DPORT_LED,HIGH);
-  //    delay(130);
-  //    digitalWrite(DPORT_LED,LOW);
 }
 
 
